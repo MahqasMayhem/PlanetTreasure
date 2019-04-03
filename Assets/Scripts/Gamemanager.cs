6f2fb2a9bd4e492406c3d601a048b5665b120edd
Dakota Jensen <dakotajensen04@gmail.com>
2019-04-03 16:13:44 -0500

    Folder Creation

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamemanager : MonoBehaviour
+{
+    #region Variables
+    protected float gravity, playerSpeed, enemySpeed;
+    protected int lives;
+
+    private int score;
+    private ArrayList sceneTargets;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    #region Public Functions
+
+    public void DamageTarget(GameObject entity)
+    {
+
+    }
+
+    public void Kill(GameObject entity)
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+    private GameObject character;
+    private bool characterJump;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.Find("2DPlayer");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterJump = CrossPlatformInputManager.GetButtonDown("Jump");
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}

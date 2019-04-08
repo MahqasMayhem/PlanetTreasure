594d5962cd0024c0e9423f6ae14e4b055f15a200
Dakota <dakotajensen04@gmail.com>
2019-04-08 17:17:29 -0500

    LaserMonkey Update wip

--- Assets/Scripts/LaserMonkey.cs
+++ Assets/Scripts/LaserMonkey.cs
@@ -6,63 +6,111 @@ public class LaserMonkey : MonoBehaviour
 {
     #region Variables
     public float moveRadius;
+    public float viewRadius;
     public float speed;
 
     private int direction;
-    private bool visible;
     private Transform Weapon;
     private Rigidbody2D rb;
     private Vector3 target;
     private GameObject player;
-
+    private float spriteScaleX, range;
+    private bool lockAim, isAiming;
 
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         direction = 1;
-        visible = false;
         Weapon = transform.Find("Weapon");
         rb = GetComponent<Rigidbody2D>();
+        spriteScaleX = this.transform.localScale.x;
         player = GameObject.Find("Player");
+        lockAim = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-        TargetEntity(player);
+        if (Weapon.GetComponent<SpriteRenderer>().isVisible == true && lockAim != true)
+        {
+            if (viewRadius == 0)
+            {
+                TargetEntity(player);
+            }
+            else
+            {
+                TargetEntity(viewRadius);
+            }
+        }
+
     }
 
     #region Control Functions
-    private void Move()
+    private void Move() //TODO# Actually write this method
     {
         rb.velocity = new Vector2(speed * direction, rb.velocity.y);
 
     }
-    private void ChangeDirection() //TODO# flip the sprite and weapon together. 
+    private void ChangeDirection(int facing) //TODO# flip the sprite and weapon together. 
     {
-        direction = direction * -1;
-        this.GetComponent<SpriteRenderer>().flipX = !this.GetComponent<SpriteRenderer>().flipX;
-    }
-
-
-    private void TargetEntity(GameObject entity)
-    {
-        if (Weapon.GetComponent<SpriteRenderer>().isVisible == true)
+        direction = facing;
+        //this.GetComponent<SpriteRenderer>().flipX = !this.GetComponent<SpriteRenderer>().flipX;
+        if (facing == 1)
         {
-        Weapon.transform.right = entity.transform.position - Weapon.transform.position;
-            if (Weapon.transform.rotation.eulerAngles.z < -90 || Weapon.transform.rotation.eulerAngles.z > 90)
-            {
-                ChangeDirection();
-            }
+            this.transform.localScale = new Vector3(1.771533f, this.transform.localScale.y, this.transform.localScale.z);
         }
-        
-
+        else
+        {
+            this.transform.localScale = new Vector3(-1.771533f, this.transform.localScale.y, this.transform.localScale.z);
+        }
     }
 
-    private void FireBeam()
+
+    private void TargetEntity(GameObject entity) //Overloaded function. Will only be used if viewRadius is 0
     {
+
+        isAiming = true;
+        Weapon.transform.right = entity.transform.position - Weapon.transform.position;
+            if (entity.transform.position.x < this.transform.position.x)
+            {
+                ChangeDirection(-1);
+            }
+            else
+            {
+                ChangeDirection(1);
+            }
+        
+        
+    }
+
+    private void TargetEntity(float radius) //Overloaded function. Will be used if viewRadius is not 0
+    {
+        Debug.Log("Using overloaded control method", this);
+
+        if (this.GetComponent<SpriteRenderer>().isVisible == false && isAiming == true)
+        {
+            isAiming = false;
+        }
+       else if (CalcRange(player, range) < viewRadius)
+        {
+            TargetEntity(player);
+        }
+    }
+    
+
+    private float CalcRange(GameObject entity, float range)
+    {
+        range = Vector2.Distance(entity.transform.position, this.transform.position);
+        //Debug.Log(range);
+        return range;
+    }
+
+    private void FireBeam() //TODO: Begin fire sequence. Flash beam, countdown to fire, then fire. Raycast to calculate hit. Reset lockAim and isFiring
+    {
+        lockAim = true;
+        Debug.Log("Firing laser!", this);
 
     }
     #endregion

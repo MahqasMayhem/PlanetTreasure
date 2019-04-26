9d66f7759528c34b06d5141eb24a9c918113ffaf
Dakota <dakotajensen04@gmail.com>
2019-04-26 16:29:49 -0500

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/ExplodingBarrel.cs
+++ Assets/Scripts/ExplodingBarrel.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class ExplodingBarrel : MonoBehaviour
 {
     public float explosionSize;
+    private bool ignited;
     private Animator anim;
     private SpriteRenderer sr;
     private Color blank, flash;
     private RaycastHit2D hit;
     private ParticleSystem ps;
+    private AudioSource sound;
     // Start is called before the first frame update
     void Start()
     {
         anim = this.GetComponent<Animator>();
         sr = this.GetComponent<SpriteRenderer>();
         ps = this.GetComponent<ParticleSystem>();
+        sound = this.GetComponent<AudioSource>();
         blank = Color.white;
         flash = new Color(255, 98, 98);
         ps.Stop();
+        ignited = false;
 
     }
 
@@ -30,19 +34,16 @@ public class ExplodingBarrel : MonoBehaviour
 
     public void OnDamage()
     {
+        if (!ignited)
         StartCoroutine("Explode");
     }
 
    private IEnumerator Explode()
     {
-      for (int i = 0; i < 5; i++)
-      {
-            sr.color = blank;
-            yield return new WaitForSeconds(0.8f);
-            sr.color = flash;
-             yield return new WaitForSeconds(0.8f);
-            ps.Play();
-      }
+        ignited = true;
+        gameObject.GetComponent<Animator>().SetTrigger("ignite");
+        sound.Play();
+        yield return new WaitForSeconds(5f);
       foreach ( RaycastHit2D hit in Physics2D.BoxCastAll(this.gameObject.transform.position, new Vector2(explosionSize,explosionSize), 0f, new Vector2(0,0), 1f))
       {
             if (!hit.collider.gameObject.CompareTag("Player"))

570fe13efb0ea534976c34ff0ee9d16a14929460
Ferrell31 <49007370+Ferrell31@users.noreply.github.com>
2019-04-26 04:11:48 -0500

    Added WinText and tried to fix scoring problem.
    For some reason the score will either go up by 2 when you pick up a gem, or it will go up bby none at all. No idea what to do.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TextMeshProUGUI text;
     int score;
+    public Text winText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,16 @@ public class ScoreManager : MonoBehaviour
         {
             instance = this;
         }
+        winText.text = "";
     }
 
     public void Changescore(int coinValue)
     {
         score += coinValue;
         text.text = "X" + score.ToString();
+        if (score >= 12)
+        {
+            winText.text = "Balanced, as all games should be....";
+        }
     }
 }
